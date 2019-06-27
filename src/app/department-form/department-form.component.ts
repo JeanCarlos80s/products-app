a324316dd1a46fd51fd92b6abac1710ae865f846
jeancarlos80s <jeancarlos80s@gmail.com>
2019-06-27 19:22:29 -0300

    first commit

---
+++ src/app/department-form/department-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-form',
+  templateUrl: './department-form.component.html',
+  styleUrls: ['./department-form.component.css']
+})
+export class DepartmentFormComponent implements OnInit {
+
+  depName: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  save(){}
+
+  clear(){}
+}
